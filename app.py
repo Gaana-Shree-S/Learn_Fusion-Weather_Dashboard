aac6a910c777814bcde069d9362c92bf4aa72b81
Gaana-Shree-S <146619352+Gaana-Shree-S@users.noreply.github.com>
2024-09-29 11:27:15 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import requests
+from configparser import ConfigParser
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # For flashing messages (optional)
+
+# Extracting key from the online API to obtain weather updates
+config_file = "config.ini"
+config = ConfigParser()
+config.read(config_file)
+api_key = "3c91f2ad78ccc17451c13dd68f107148"  # Replace this with your own API key
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+
+def get_weather(city):
+    result = requests.get(url.format(city, api_key))
+    if result:
+        sample = result.json()
+        city = sample['name']
+        country = sample['sys']['country']
+        temp_kelvin = sample['main']['temp']
+        temp_celsius = temp_kelvin - 273.15
+        weather_desc = sample['weather'][0]['main']
+        return {'city': city, 'country': country, 'temp_celsius': temp_celsius, 'weather_desc': weather_desc}
+    else:
+        return None
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city = request.form.get('city')
+        weather = get_weather(city)
+        if weather:
+            return render_template('index.html', weather=weather)
+        else:
+            flash("Cannot find the city {}".format(city))
+            return redirect(url_for('index'))
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
